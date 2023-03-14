736241fc81f4099208ba6d7fd560625233407c1c
hendisantika <hendisantika@yahoo.co.id>
2023-03-14 07:41:34 +0700

    #23 - Modify SpringBootKafkaTestApplicationTests ✏️📗:octocat:

--- src/test/java/com/hendisantika/springbootkafkatest/SpringBootKafkaTestApplicationTests.java
+++ src/test/java/com/hendisantika/springbootkafkatest/SpringBootKafkaTestApplicationTests.java
@@ -1,13 +1,38 @@
 package com.hendisantika.springbootkafkatest;
 
+import com.hendisantika.springbootkafkatest.kafka.producer.UserKafkaProducer;
+import com.hendisantika.springbootkafkatest.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootKafkaTestApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:test");
+        registry.add("spring.datasource.driverClassName", () -> "org.h2.Driver");
+        registry.add("spring.datasource.username", () -> "root");
+        registry.add("spring.datasource.password", () -> "secret");
+        registry.add("spring.flyway.enabled", () -> "false");
     }
 
+    @Autowired
+    private UserKafkaProducer userKafkaProducer;
+
+    @MockBean
+    private UserService userService;
 }
