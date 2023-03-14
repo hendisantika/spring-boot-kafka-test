9c332aaeeda59f1d1f149b177dcac4dea698fd9d
hendisantika <hendisantika@yahoo.co.id>
2023-03-14 07:35:43 +0700

    #16 - Add logKafkaMessages in UserKafkaConsumer ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootkafkatest/kafka/consumer/UserKafkaConsumer.java
+++ src/main/java/com/hendisantika/springbootkafkatest/kafka/consumer/UserKafkaConsumer.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootkafkatest.kafka.consumer;
 
+import com.hendisantika.springbootkafkatest.dto.UserDto;
 import com.hendisantika.springbootkafkatest.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,4 +28,15 @@ import org.springframework.stereotype.Component;
 public class UserKafkaConsumer {
 
     private final UserService userService;
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}",
+            concurrency = "${spring.kafka.consumer.level.concurrency:3}")
+    public void logKafkaMessages(@Payload UserDto user,
+                                 @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                                 @Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition,
+                                 @Header(KafkaHeaders.OFFSET) Long offset) {
+        log.info("Received a message contains a user information with id {}, from {} topic, " +
+                "{} partition, and {} offset", user.getUuid(), topic, partition, offset);
+        userService.save(user);
+    }
 }
