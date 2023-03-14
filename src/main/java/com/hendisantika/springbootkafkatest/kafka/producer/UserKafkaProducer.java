082657389d82046c4e917065e1ae35f9303f7c1b
hendisantika <hendisantika@yahoo.co.id>
2023-03-14 07:37:09 +0700

    #17 - Add UserKafkaProducer ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootkafkatest/kafka/producer/UserKafkaProducer.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootkafkatest.kafka.producer;
+
+import com.hendisantika.springbootkafkatest.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/14/23
+ * Time: 07:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserKafkaProducer {
+
+    private final KafkaTemplate<String, UserDto> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.name}")
+    private String topic;
+
+    @Value("${spring.kafka.replication.factor:1}")
+    private int replicationFactor;
+
+    @Value("${spring.kafka.partition.number:1}")
+    private int partitionNumber;
+}
