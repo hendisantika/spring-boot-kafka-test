b400114275a468f3f87012cada8486727888de38
hendisantika <hendisantika@yahoo.co.id>
2023-03-15 09:15:34 +0700

    #30 - Add UserKafkaProducerTest ✏️📗:octocat:

---
+++ src/test/java/com/hendisantika/springbootkafkatest/kafka/producer/UserKafkaProducerTest.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootkafkatest.kafka.producer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/15/23
+ * Time: 09:15
+ * To change this template use File | Settings | File Templates.
+ */
+@EmbeddedKafka
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserKafkaProducerTest {
+
+    private BlockingQueue<ConsumerRecord<String, String>> records;
+
+    private KafkaMessageListenerContainer<String, String> container;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private UserKafkaProducer producer;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+}
