c73eb9c7ad98a64f4d196ad94a8e07e936c45b60
hendisantika <hendisantika@yahoo.co.id>
2023-03-15 09:21:08 +0700

    #38 - Add kafkaProperties in UserServiceTest ✏️📗:octocat:

--- src/test/java/com/hendisantika/springbootkafkatest/service/UserServiceTest.java
+++ src/test/java/com/hendisantika/springbootkafkatest/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springbootkafkatest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -27,5 +30,17 @@ public class UserServiceTest {
     @Container
     static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0-debian"));
 
+    @Autowired
+    private UserService userService;
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("spring.datasource.url", () -> mySQLContainer.getJdbcUrl());
+        registry.add("spring.datasource.driverClassName", () -> mySQLContainer.getDriverClassName());
+        registry.add("spring.datasource.username", () -> mySQLContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mySQLContainer.getPassword());
+        registry.add("spring.flyway.enabled", () -> "true");
+    }
 
 }
