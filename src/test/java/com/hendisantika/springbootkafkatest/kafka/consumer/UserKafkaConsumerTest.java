931fe158ea4030b6de7b87d3defe058c758c1597
hendisantika <hendisantika@yahoo.co.id>
2023-03-15 09:10:19 +0700

    #26 - Add UserKafkaConsumerTest ✏️📗:octocat:

---
+++ src/test/java/com/hendisantika/springbootkafkatest/kafka/consumer/UserKafkaConsumerTest.java
@@ -0,0 +1,51 @@
+package com.hendisantika.springbootkafkatest.kafka.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springbootkafkatest.entity.User;
+import com.hendisantika.springbootkafkatest.service.UserService;
+import org.apache.kafka.clients.producer.Producer;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/15/23
+ * Time: 09:08
+ * To change this template use File | Settings | File Templates.
+ */
+@EmbeddedKafka
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserKafkaConsumerTest {
+
+    private final String TOPIC_NAME = "com.madadipouya.kafka.user";
+    @Captor
+    ArgumentCaptor<User> userArgumentCaptor;
+    @Captor
+    ArgumentCaptor<String> topicArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Integer> partitionArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Long> offsetArgumentCaptor;
+    private Producer<String, String> producer;
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @SpyBean
+    private UserKafkaConsumer userKafkaConsumer;
+    @MockBean
+    private UserService userService;
+
+}
