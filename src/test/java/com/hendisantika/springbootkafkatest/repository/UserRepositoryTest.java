7e336e2e64d8215e0480399ea45942941e326469
hendisantika <hendisantika@yahoo.co.id>
2023-03-15 09:17:46 +0700

    #35 - Add UserRepositoryTest ✏️📗:octocat:

---
+++ src/test/java/com/hendisantika/springbootkafkatest/repository/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootkafkatest.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/15/23
+ * Time: 09:17
+ * To change this template use File | Settings | File Templates.
+ */
+@DataJpaTest
+@DirtiesContext
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+}
